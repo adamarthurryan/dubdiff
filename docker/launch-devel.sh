4fe73b91bc1c1df9f0cf16268ed583a1acb73db8
Adam Brown <adamarthurryan@gmail.com>
2015-04-18 10:05:32 -0400

    move docker launch scripts to docker folder

--- docker/launch-devel.sh
+++ docker/launch-devel.sh
@@ -16,4 +16,6 @@ if docker inspect -f {{.Name}} data-docker-home > /dev/null
 
 fi
 
-docker run -it --rm -e "PORT=8080" --volumes-from=data-docker-home -v ${PWD}:/working -w /working -p 80:8080 adamarthurryan/mean grunt serve:dist
+echo Run this script from the project root folder!
+
+docker run -it --rm --volumes-from=data-docker-home -v ${PWD}:/working -w /working -p 9000:9000 -p 35729:35729 adamarthurryan/mean grunt serve
